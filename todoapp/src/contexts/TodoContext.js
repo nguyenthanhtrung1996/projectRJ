cd2bc867db2664296fc1a1dfd40a32901fc86e38
Trung <trungnguyen00079@gmail.com>
2021-04-05 11:32:24 +0700

    update context

--- todoapp/src/contexts/TodoContext.js
+++ todoapp/src/contexts/TodoContext.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
 import {toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 toast.configure()
